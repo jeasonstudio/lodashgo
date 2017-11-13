6864999071810e0f3ca87fc866c0bb31e08161f5
Jeason <zhaojitong@xiaomi.com>
2017-11-13 13:23:50 +0800

    add UnSafeChunk

--- Array/chunk.go
+++ Array/chunk.go
@@ -1,10 +1,15 @@
 package Array
 
-// Chunk Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
-func Chunk(s []interface{}, size int) ([][]interface{}, error) {
-	result := [][]interface{} {}
+import (
+	"errors"
+)
+
+// IntChunk Creates an array of elements split into groups the length of size.
+// If array can't be split evenly, the final chunk will be the remaining elements.
+func IntChunk(s []int, size int) ([][]int, error) {
+	var result [][]int
 	for index := 0; index < len(s); index += size {
-		temp := [] interface{} {}
+		var temp []int
 		for indexForSize := 0; indexForSize < size; indexForSize++ {
 			if (index + indexForSize < len(s)) {
 				temp = append(temp, s[index + indexForSize])
@@ -13,4 +18,53 @@ func Chunk(s []interface{}, size int) ([][]interface{}, error) {
 		result = append(result, temp)
 	}
 	return result, nil
+}
+
+// UnSafeChunk Creates an array of elements split into groups the length of size.
+// If array can't be split evenly, the final chunk will be the remaining elements.
+// The difference between Chunk is UnSafeChunk accept any kind type of Array.
+func UnSafeChunk(s interface{}, size int) ([]interface{}, error) {
+	var result []interface{}
+	var err error
+	// Types support
+	switch s.(type) {
+			case []int:
+				selfSlice := s.([]int)
+				for index := 0; index < len(selfSlice); index += size {
+					temp := []int {}
+					for indexForSize := 0; indexForSize < size; indexForSize++ {
+						if (index + indexForSize < len(selfSlice)) {
+							temp = append(temp, selfSlice[index + indexForSize])
+						}
+					}
+					result = append(result, temp)
+				}
+			case []string:
+				selfSlice := s.([]string)
+				for index := 0; index < len(selfSlice); index += size {
+					temp := []string {}
+					for indexForSize := 0; indexForSize < size; indexForSize++ {
+						if (index + indexForSize < len(selfSlice)) {
+							temp = append(temp, selfSlice[index + indexForSize])
+						}
+					}
+					result = append(result, temp)
+				}
+			case []interface{}:
+				selfSlice := s.([]interface{})
+				for index := 0; index < len(selfSlice); index += size {
+					temp := []interface{} {}
+					for indexForSize := 0; indexForSize < size; indexForSize++ {
+						if (index + indexForSize < len(selfSlice)) {
+							temp = append(temp, selfSlice[index + indexForSize])
+						}
+					}
+					result = append(result, temp)
+				}
+			default:
+				// Not match any kind of [int, string, interface{}]
+				result = nil
+				err = errors.New("Error: The first arg should be some kind of `Array`")
+		}
+	return result, err
 }
--- main.go
+++ main.go
@@ -7,19 +7,32 @@ import (
 
 func main()  {
 	fmt.Println("Hi, guys!")
-	a := [] int {2}
-	b := [] string {"s", "aa"}
-	re, _ := Array.Concat(a, b, 3, "sss", true)
-	// ree, _ := Array.Chunk(b, 2)
-	fmt.Println(re)
-	t1 := []uint64 {}
-	t2 := []string {}
-	test := []interface{} {t1, t2}
-	for _, tests := range test {
-		switch t := tests.(type) {
-			default:
-				fmt.Printf("%T\n", t)
+	// a := [] int {2}
+	// b := [] string {"s", "aa"}
+	// re, _ := Array.Concat(a, b, 3, "sss", true)
+	// // ree, _ := Array.Chunk(b, 2)
+	// fmt.Println(re)
+	// t1 := []uint64 {}
+	// t2 := []string {}
+	// test := []interface{} {t1, t2}
+	// for _, tests := range test {
+	// 	switch t := tests.(type) {
+	// 		default:
+	// 			fmt.Printf("%T\n", t)
+	// 	}
+	// }
+	a := []int32 {1, 2, 3}
+	res, err := Array.UnSafeChunk(a, 2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(res)
+	for _, ts := range res {
+		switch t := ts.(type) {
+		default:
+			fmt.Printf("%T", t)
 		}
 	}
+
 	return
 }
