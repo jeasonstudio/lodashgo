fd600f43de684465944f8245b935ca41d1b2b6e8
Jeason <zhaojitong@xiaomi.com>
2017-11-13 13:40:41 +0800

    update tests

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ lodashgo is a GoLang library as lodash.js to JavaScript
 ```
 godoc -http=:6060
 ```
+
+### gotest
+```
+go test loadshgo/Test
+```
--- Test/Array_test.go
+++ Test/Array_test.go
@@ -5,15 +5,15 @@ import (
 	"testing"
 )
 
-func Test_Array_Chunk(t *testing.T)  {
-	t1 := []interface{} {1, 2, nil, "sss", true}
-	r1, _ := Array.Chunk(t1, 2)
-	r2, _ := Array.Chunk(t1, 3)
-	r3, _ := Array.Chunk(t1, 4)
-	r4, _ := Array.Chunk(t1, 7)
-	if len(r1) != 3 {
+func Test_Array_IntChunk(t *testing.T)  {
+	t1 := []int {1, 2, 3, 4, 5, 7, 9}
+	r1, _ := Array.IntChunk(t1, 2)
+	r2, _ := Array.IntChunk(t1, 3)
+	r3, _ := Array.IntChunk(t1, 4)
+	r4, _ := Array.IntChunk(t1, 7)
+	if len(r1) != 4 {
 		t.Error("Some thing error")
-	} else if len(r2) != 2 {
+	} else if len(r2) != 3 {
 		t.Error("Some thing error")
 	} else if len(r3) != 2 {
 		t.Error("Some thing error")
@@ -24,12 +24,12 @@ func Test_Array_Chunk(t *testing.T)  {
 	}
 }
 
-func Test_Array_Compact(t *testing.T)  {
-	t1 := []interface{} {1, false, nil, "sss", 0, true, ""}
-	r, _ := Array.Compact(t1)
-	if len(r) != 3 {
-		t.Error("error")
-	} else {
-		t.Log("Test_Array_Compact Passed")
-	}
-}
+// func Test_Array_Compact(t *testing.T)  {
+// 	t1 := []interface{} {1, false, nil, "sss", 0, true, ""}
+// 	r, _ := Array.Compact(t1)
+// 	if len(r) != 3 {
+// 		t.Error("error")
+// 	} else {
+// 		t.Log("Test_Array_Compact Passed")
+// 	}
+// }
