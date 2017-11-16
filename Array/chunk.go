f6fbe89e070177a405c70a32eb9b916d5db09490
Jeason <zhaojitong@xiaomi.com>
2017-11-16 15:50:47 +0800

    add

--- Array/chunk.go
+++ Array/chunk.go
@@ -11,8 +11,8 @@ func IntChunk(s []int, size int) ([][]int, error) {
 	for index := 0; index < len(s); index += size {
 		var temp []int
 		for indexForSize := 0; indexForSize < size; indexForSize++ {
-			if (index + indexForSize < len(s)) {
-				temp = append(temp, s[index + indexForSize])
+			if index+indexForSize < len(s) {
+				temp = append(temp, s[index+indexForSize])
 			}
 		}
 		result = append(result, temp)
@@ -28,43 +28,62 @@ func UnSafeChunk(s interface{}, size int) ([]interface{}, error) {
 	var err error
 	// Types support
 	switch s.(type) {
-			case []int:
-				selfSlice := s.([]int)
-				for index := 0; index < len(selfSlice); index += size {
-					temp := []int {}
-					for indexForSize := 0; indexForSize < size; indexForSize++ {
-						if (index + indexForSize < len(selfSlice)) {
-							temp = append(temp, selfSlice[index + indexForSize])
-						}
-					}
-					result = append(result, temp)
+	case []int:
+		selfSlice := s.([]int)
+		for index := 0; index < len(selfSlice); index += size {
+			temp := []int{}
+			for indexForSize := 0; indexForSize < size; indexForSize++ {
+				if index+indexForSize < len(selfSlice) {
+					temp = append(temp, selfSlice[index+indexForSize])
 				}
-			case []string:
-				selfSlice := s.([]string)
-				for index := 0; index < len(selfSlice); index += size {
-					temp := []string {}
-					for indexForSize := 0; indexForSize < size; indexForSize++ {
-						if (index + indexForSize < len(selfSlice)) {
-							temp = append(temp, selfSlice[index + indexForSize])
-						}
-					}
-					result = append(result, temp)
-				}
-			case []interface{}:
-				selfSlice := s.([]interface{})
-				for index := 0; index < len(selfSlice); index += size {
-					temp := []interface{} {}
-					for indexForSize := 0; indexForSize < size; indexForSize++ {
-						if (index + indexForSize < len(selfSlice)) {
-							temp = append(temp, selfSlice[index + indexForSize])
-						}
-					}
-					result = append(result, temp)
-				}
-			default:
-				// Not match any kind of [int, string, interface{}]
-				result = nil
-				err = errors.New("Error: The first arg should be some kind of `Array`")
+			}
+			result = append(result, temp)
 		}
+	case []string:
+		selfSlice := s.([]string)
+		for index := 0; index < len(selfSlice); index += size {
+			temp := []string{}
+			for indexForSize := 0; indexForSize < size; indexForSize++ {
+				if index+indexForSize < len(selfSlice) {
+					temp = append(temp, selfSlice[index+indexForSize])
+				}
+			}
+			result = append(result, temp)
+		}
+	case []interface{}:
+		selfSlice := s.([]interface{})
+		for index := 0; index < len(selfSlice); index += size {
+			temp := []interface{}{}
+			for indexForSize := 0; indexForSize < size; indexForSize++ {
+				if index+indexForSize < len(selfSlice) {
+					temp = append(temp, selfSlice[index+indexForSize])
+				}
+			}
+			result = append(result, temp)
+		}
+	default:
+		// Not match any kind of [int, string, interface{}]
+		result = nil
+		err = errors.New("Error: The first arg should be some kind of `Array`")
+	}
 	return result, err
 }
+
+// // 如果我想给 slice 写一个通用的方法函数 (比如 chunk)
+// // 在没有泛型的情况下是不是只能想下面这样 blabla 每种类型都实现一遍?
+// func IntChunk(s []int, size int) ([][]int, error)
+// func Int8Chunk(s []int8, size int) ([][]int8, error)
+// func Int32Chunk(s []int32, size int) ([][]int32, error)
+// func Int64Chunk(s []int64, size int) ([][]int64, error)
+// func UintChunk(s []uint, size int) ([][]uint, error)
+// func Uint8Chunk(s []uint8, size int) ([][]uint8, error)
+// func Uint32Chunk(s []uint32, size int) ([][]uint32, error)
+// func Uint64Chunk(s []uint64, size int) ([][]uint64, error)
+// func Float32Chunk(s []float32, size int) ([][]float32, error)
+// func Float64Chunk(s []float64, size int) ([][]float64, error)
+// func StringChunk(s []string, size int) ([][]string, error)
+
+// // 实现成 interface{} 就丢失了静态类型检查, 感觉不太好 -_-
+// func Chunk(s interface{}, size int) ([]interface{}, error)
+
+// // 这样说对吗?
--- main.go
+++ main.go
@@ -1,36 +1,17 @@
 package main
 
-import (
-	"errors"
-	"lodashgo/Array"
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Hi, guys!")
-	x := []int {1,2,3,4}
-	a, _ := Array.IntReduce(x, reduceInt)
-	y := []string {"a", "b", "c", "d"}
-	b, _ := Array.StringReduce(y, reduceString)
-	fmt.Println(a)
-	fmt.Println(b)
+	var map1 map[string]interface{}
+	map1["shit"] = func() {
+		fmt.Println("shit")
+	}
+	fmt.Println(map1)
+	switch map1["shit"].(type) {
+	case func():
+		map1["shit"].(func())()
+	}
 	return
-}
-
-func reduceInt(accumulator interface{}, currentValue, currentIndex int, array []int) interface{} {
-	switch accumulator.(type) {
-		case int:
-			return accumulator.(int) + currentValue
-		default:
-			return errors.New("")
-	}
-}
-
-func reduceString(accumulator interface{}, currentValue string, currentIndex int, array []string) interface{} {
-	switch accumulator.(type) {
-		case string:
-			return accumulator.(string) + currentValue
-		default:
-			return errors.New("")
-	}
 }
